1196095bacd83fbc9ec73419b4de3b8982ee2c8a
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-02-25 20:03:51 +0530

    project done

--- server/index.js
+++ server/index.js
@@ -8,15 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import authRoutes from "./routes/auth.js.js";
-import userRoutes from "./routes/users.js.js";
-import postRoutes from "./routes/posts.js.js";
-import { register } from "./controllers/auth.js.js";
-import { createPost } from "./controllers/posts.js.js";
-import { verifyToken } from "./middleware/auth.js.js";
-import User from "./models/User.js.js";
-import Post from "./models/Post.js.js";
-import { users, posts } from "./data/index.js.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
